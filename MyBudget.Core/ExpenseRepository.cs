b70a5770e2e74da695738f1a6b96c25984a1ffe5
Benjamin Andoh <benoo.andoh@gmail.com>
2026-07-06 15:16:45 -0400

    acorrect e.MonthlyImpact

--- MyBudget.Core/ExpenseRepository.cs
+++ MyBudget.Core/ExpenseRepository.cs
@@ -4,7 +4,52 @@ using System.Text;
 
 namespace MyBudget.Core
 {
-    internal class ExpenseRepository
+    public class ExpenseRepository : IExpenseRepository
     {
+        private readonly List<Expense> _expenses;
+        private readonly IExpenseStore _expenseStore;
+
+        public ExpenseRepository(IExpenseStore store) 
+        {
+            _expenseStore = store;
+            _expenses = store.Load().ToList();
+        }
+
+
+        public void Add(Expense expense)
+        {
+            if (expense == null)
+            {
+                throw new ArgumentNullException(nameof(expense));
+            }
+            _expenses.Add(expense);
+        }
+
+        public IReadOnlyList<Expense> GetAll()
+        {
+            return _expenses.OrderBy(e=>e.Date).ToList();
+        }
+
+        public IReadOnlyList<Expense> InCategory(ExpenseCategory category)
+        {
+            return _expenses.Where(e => e.Category == category).ToList();
+        }
+
+        public void Save()
+        {
+            _expenseStore.Save(_expenses);
+        }
+
+        public decimal Total()
+        {
+            return _expenses.Sum(e => e.MonthlyImpact);
+        }
+
+        public IReadOnlyDictionary<ExpenseCategory, decimal> TotalsByCategory()
+        {
+            return _expenses
+                .GroupBy(e => e.Category)
+                .ToDictionary(g => g.Key, g => g.Sum(e => e.MonthlyImpact));  
+        }
     }
 }
