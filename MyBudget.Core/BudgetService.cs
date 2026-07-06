5f288e982f5fe6fe06e2427c91e8c922d0206be1
Benjamin Andoh <benoo.andoh@gmail.com>
2026-07-05 23:34:21 -0400

    add budget Service

---
+++ MyBudget.Core/BudgetService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBudget.Core
+{
+    public class BudgetService
+    {
+        decimal MonthlyLimit { get; private set; }
+    }
+}
